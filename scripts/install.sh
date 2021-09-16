aede39bd636127de929aa221934921331f87280d
jon.liu <jon.liu@yunzhihui.com>
2021-09-16 10:47:48 +0800

    增加服务控制脚本

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,15 +9,30 @@ PROJECT_FOLDER="$(dirname ${CURRENT_DIR})"
 # 解决openssl依赖的问题
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PROJECT_FOLDER}/component/env/lib/
 
+PYTHON3="${PROJECT_FOLDER}/component/env/bin/python3"
+OMP_SCRIPT="${PROJECT_FOLDER}/scripts/omp"
+
 # 平台端安装逻辑
 function install_omp() {
-
+  update_conf_path="${PROJECT_FOLDER}/scripts/source/update_conf.py"
+  run_user=$(whoami)
+  $PYTHON3 $update_conf_path $1 $run_user
+  if [[ $? -ne 0 ]]; then
+    echo "OMP配置更新失败"
+    exit 1
+  fi
+  bash $OMP_SCRIPT start
 }
 
 # 监控端安装逻辑
 function install_monitor_server() {
-
+  echo ""
 }
 
-install_omp
-install_monitor_server
+if [[ $# -eq 0 ]]; then
+  echo "Please use: 'bash install.sh local_ip'"
+  exit 1
+else
+  install_omp "$@"
+  install_monitor_server "$@"
+fi
