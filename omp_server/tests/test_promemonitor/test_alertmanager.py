045fa607e292a5b09db36f347c3baaa831c2292e
lingyang.guo <lingyang.guo@cloudwise.com>
2021-09-24 20:12:23 +0800

    修正promemonitor模块单元测试存在的问题

--- omp_server/tests/test_promemonitor/test_alertmanager.py
+++ omp_server/tests/test_promemonitor/test_alertmanager.py
@@ -10,10 +10,10 @@ class TestAlertmanager(TestCase):
 
     def test_set_maintain(self):
         alertmanager = Alertmanager()
-        maintain_id = alertmanager.add_setting(value='10.0.3.89', name='instance')
-        TestCase.assertIsNone(maintain_id, '添加维护失败')
+        maintain_id = alertmanager.add_setting(value='10.0.3.90', name='instance')
+        TestCase.assertIsNotNone(maintain_id, '添加维护失败')
 
     def test_delete_maintain(self):
         alertmanager = Alertmanager()
-        delete_result = alertmanager.delete_setting('')
-        TestCase.assertTrue(delete_result, '删除维护失败')
+        delete_result = alertmanager.delete_setting('f45b3f5d-bea4-40f5-bcb6-207fad21a563')
+        self.assertTrue(delete_result, '删除维护失败')
--- omp_server/tests/test_promemonitor/test_prometheus.py
+++ omp_server/tests/test_promemonitor/test_prometheus.py
@@ -6,7 +6,10 @@ from promemonitor.prometheus import Prometheus
 class TestPrometheus(TestCase):
 
     def setup(self):
-        self.host_list = [
+        pass
+
+    def test_get_prometheus_info(self):
+        host_list = [
             {
                 'ip': '10.0.3.71',
                 'data_folder': '/boot',
@@ -32,15 +35,13 @@ class TestPrometheus(TestCase):
                 'data_disk_usage': 0
             }
         ]
-        self.correct_host_info_data = [
+        correct_host_info_data = [
             {'ip': '10.0.3.71', 'data_folder': '/boot', 'cpu_usage': 1, 'mem_usage': 5, 'root_disk_usage': 11,
              'data_disk_usage': 15, 'cpu_status': 'warning', 'mem_status': 'warning', 'root_disk_status': 'warning'},
             {'ip': '10.0.9.60', 'data_disk': '/boot', 'cpu_usage': None, 'mem_usage': None, 'root_disk_usage': None,
              'data_disk_usage': 0, 'cpu_status': None, 'mem_status': None, 'root_disk_status': None},
             {'ip': '10.0.9.61', 'data_disk': '/boot', 'cpu_usage': None, 'mem_usage': None, 'root_disk_usage': None,
              'data_disk_usage': 0, 'cpu_status': None, 'mem_status': None, 'root_disk_status': None}]
-
-    def test_get_prometheus_info(self):
         prometheus = Prometheus()
-        host_info = prometheus.get_host_info(self.host_list)
-        TestCase.assertListEqual(host_info, self.correct_host_info_data, list())
+        host_info = prometheus.get_host_info(host_list)
+        self.assertListEqual(host_info, correct_host_info_data)
