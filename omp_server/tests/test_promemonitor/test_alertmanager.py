657c97be7d67ee4286a09d9574ceecd47dee1e99
lingyang.guo <lingyang.guo@cloudwise.com>
2021-09-26 16:25:41 +0800

    解决单元测试失败的问题

--- omp_server/tests/test_promemonitor/test_alertmanager.py
+++ omp_server/tests/test_promemonitor/test_alertmanager.py
@@ -1,20 +1,28 @@
 from django.test import TestCase
 
 from promemonitor.alertmanager import Alertmanager
+from db_models.models import MonitorUrl
 
 
 class TestAlertmanager(TestCase):
 
     def setUp(self):
-        pass
+        MonitorUrl.objects.create(
+            name='alertmanager', monitor_url='10.0.3.66:19013')
 
     def test_set_maintain(self):
         alertmanager = Alertmanager()
         maintain_id = alertmanager.add_setting(
-            value='10.0.3.90', name='instance')
+            value='10.0.3.91', name='instance')
         TestCase.assertIsNotNone(maintain_id, '添加维护失败')
+        return maintain_id
 
-    # def test_delete_maintain(self):
-    #     alertmanager = Alertmanager()
-    #     delete_result = alertmanager.delete_setting('f45b3f5d-bea4-40f5-bcb6-207fad21a563')
-    #     self.assertTrue(delete_result, '删除维护失败')
+    def test_delete_maintain(self):
+        alertmanager = Alertmanager()
+        maintain_id = self.test_set_maintain()
+        delete_result = alertmanager.delete_setting(maintain_id)
+        self.assertTrue(delete_result, '删除维护失败')
+
+    def tearDown(self):
+        # MonitorUrl.objects.delete(name='alertmanager')
+        pass
