9d3d31f3eb4912226aee714eb6f78b21ca6c2453
lingyang.guo <lingyang.guo@cloudwise.com>
2021-09-26 20:45:15 +0800

    增加全局维护和主机批量维护的单元测试

--- omp_server/tests/test_promemonitor/test_alertmanager.py
+++ omp_server/tests/test_promemonitor/test_alertmanager.py
@@ -10,17 +10,31 @@ class TestAlertmanager(TestCase):
         MonitorUrl.objects.create(
             name='alertmanager', monitor_url='10.0.3.66:19013')
 
-    def test_set_maintain(self):
+    def test_set_maintain_by_host_list(self):
         alertmanager = Alertmanager()
-        maintain_id = alertmanager.add_setting(
-            value='10.0.3.91', name='instance')
-        TestCase.assertIsNotNone(maintain_id, '添加维护失败')
-        return maintain_id
+        host_list = [
+            {
+                'ip': '10.0.3.71',
+                'data_folder': '/boot',
+                'cpu_usage': 0,
+                'mem_usage': 0,
+                'root_disk_usage': 0,
+                'data_disk_usage': 0,
+            }]
+        maintain_ids = alertmanager.set_maintain_by_host_list(host_list)
+        TestCase.assertIsNotNone(maintain_ids, '添加维护失败')
+        return maintain_ids
+
+    def test_set_maintain_by_env_name(self):
+        alertmanager = Alertmanager()
+        maintain_ids = alertmanager.set_maintain_by_env_name('default')
+        TestCase.assertIsNotNone(maintain_ids, '添加维护失败')
+        return maintain_ids
 
     def test_delete_maintain(self):
         alertmanager = Alertmanager()
-        maintain_id = self.test_set_maintain()
-        delete_result = alertmanager.delete_setting(maintain_id)
+        maintain_ids = self.test_set_maintain_by_env_name()
+        delete_result = alertmanager.delete_setting(maintain_ids[0])
         self.assertTrue(delete_result, '删除维护失败')
 
     def tearDown(self):
