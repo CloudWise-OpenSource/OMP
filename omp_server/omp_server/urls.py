ca6e18268b99e8f82100f10384299c492330d980
jon.liu <jon.liu@yunzhihui.com>
2021-10-12 14:40:40 +0800

    跳转grafana视图迁移及补充单元测试代码

--- omp_server/omp_server/urls.py
+++ omp_server/omp_server/urls.py
@@ -35,8 +35,9 @@ urlpatterns_inside = [
     path("login/", JwtAPIView.as_view(), name="login"),
     path("users/", include(users_router.urls), name="users"),
     path("hosts/", include(hosts_router.urls), name="hosts"),
-    path("promemonitor/", include(promemonitor_router.urls), name="promemonitor"),
     path("appStore/", include(app_store_router.urls), name="appStore"),
+    path("promemonitor/",
+         include(promemonitor_router.urls), name="promemonitor"),
 ]
 
 urlpatterns = [
