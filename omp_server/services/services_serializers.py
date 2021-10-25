0a9af93039fb71e0920c02ddecc490eb4691cb6d
VUM <vum.si@cloudwise.com>
2021-10-25 16:43:22 +0800

    完成服务详情接口及单元测试，补充应用商店列表页实例数量部分

--- omp_server/services/services_serializers.py
+++ omp_server/services/services_serializers.py
@@ -23,17 +23,18 @@ class ServiceSerializer(serializers.ModelSerializer):
         """ 元数据 """
         model = Service
         fields = (
-            "service_instance_name", "ip", "port", "label_name", "alert_count",
+            "id", "service_instance_name", "ip", "port", "label_name", "alert_count",
             "app_type", "app_name", "app_version", "cluster_type", "service_status",
         )
 
     def get_port(self, obj):
-        """ 拼接返回端口 """
+        """ 返回服务 service_port """
         service_port_ls = json.loads(obj.service_port)
         service_port = "-"
         for info in service_port_ls:
             if info.get("service_port", None) is not None:
                 service_port = info.get("service_port")
+                break
         return service_port
 
     def get_label_name(self, obj):
@@ -63,3 +64,69 @@ class ServiceSerializer(serializers.ModelSerializer):
         if obj.service.extend_fields.get("base_env") in (True, "True"):
             alert_count = "-"
         return alert_count
+
+
+class ServiceDetailSerializer(serializers.ModelSerializer):
+    """ 服务详情序列化器 """
+
+    app_name = serializers.CharField(source="service.app_name")
+    app_version = serializers.CharField(source="service.app_version")
+    label_name = serializers.SerializerMethodField()
+    cluster_type = serializers.SerializerMethodField()
+    install_info = serializers.SerializerMethodField()
+    history = serializers.SerializerMethodField()
+
+    class Meta:
+        """ 元数据 """
+        model = Service
+        fields = (
+            "id", "service_instance_name", "app_name", "app_version", "label_name",
+            "cluster_type", "ip", "install_info", "history",
+        )
+
+    def get_install_info(self, obj):
+        """ 安装信息 """
+        result = {
+            "service_port": "-",
+            "base_dir": "-",
+            "log_dir": "-",
+            "data_dir": "-",
+            "username": "-",
+            "password": "-",
+        }
+        # 端口号
+        service_port_ls = json.loads(obj.service_port)
+        for info in service_port_ls:
+            if info.get("service_port", None) is not None:
+                result["service_port"] = info.get("service_port", "-")
+                break
+        # 应用安装参数
+        app_install_args = "[]"
+        if obj.service.app_install_args is not None:
+            app_install_args = obj.service.app_install_args
+        app_install_args = json.loads(app_install_args)
+        for app_install_info in app_install_args:
+            key = app_install_info.get("key", "")
+            if key in result.keys():
+                result[key] = app_install_info.get("default", "-")
+        return result
+
+    def get_label_name(self, obj):
+        """ 获取拼接后的标签 """
+        label_name = "-"
+        if obj.service.app_labels.exists():
+            label_name = ", ".join(
+                obj.service.app_labels.values_list("label_name", flat=True))
+        return label_name
+
+    def get_cluster_type(self, obj):
+        """ 获取集群类型 """
+        cluster_type = "-"
+        if obj.cluster is not None:
+            cluster_type = obj.cluster.cluster_type
+        return cluster_type
+
+    def get_history(self, obj):
+        """ 获取历史记录 """
+        return list(obj.servicehistory_set.values(
+            "username", "description", "result", "created"))
--- omp_server/services/urls.py
+++ omp_server/services/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
 from services.views import (
-    ServiceListView
+    ServiceListView, ServiceDetailView
 )
 
 router = DefaultRouter()
 router.register("services", ServiceListView, basename="services")
+router.register("services", ServiceDetailView, basename="services")
