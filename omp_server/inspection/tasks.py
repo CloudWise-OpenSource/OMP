1bc556771397ce861f58825b2cf79164664146e3
len.chen <len.chen@cloudwise.com>
2021-10-25 23:30:46 +0800

    巡检模块代码重构优化

--- omp_server/inspection/tasks.py
+++ omp_server/inspection/tasks.py
@@ -15,51 +15,72 @@ from db_models.models import InspectionHistory, InspectionReport
 from utils.prometheus.target_host import HostCrawl
 from utils.prometheus.prometheus import back_fill
 from utils.prometheus.target_service import target_service_run
+
 # 屏蔽celery任务日志中的paramiko日志
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 logger = get_task_logger("celery_log")
 
 
-def get_hosts_data(env, hosts, history_id, report_id):
+def get_hosts_data(env, hosts):
     """
     查询多主机prometheus数据，组装后进行反填
-    :env: 环境，例：demo
-    :hosts: 主机列表，例：[{"id":"主键id", "ip":"主机ip"}]
-    :history_id: 巡检历史表id，例：1
-    :report_id: 巡检报告表id，例：1
+    :env: 环境queryset
+    :hosts: 主机列表，例：["主机ip"]
     """
-    # target为实例方法，目的是统一执行实例方法并统一返回值
-    target = ['rate_cpu', 'rate_memory', 'rate_max_disk', 'rate_exchange_disk',
-              'run_time', 'avg_load',
-              'total_file_descriptor', 'rate_io_wait',
-              'network_bytes_total', 'disk_io', 'run_status']
-
     temp_list = list()
-    tag_total_num = tag_error_num = 0
+    scan_info = {"host": len(hosts), "service": 0, "component": 0}  # 扫描统计
+    tag_total_num = tag_error_num = 0  # 总指标数/异常指标数
     for instance in hosts:
         temp = dict()
         # 主机 prometheus 数据请求
-        h_w_obj = HostCrawl(env=env, instance=instance.get('ip'))
-        h_w_obj.run(target=target)
-        tag_total_num += h_w_obj.tag_total_num
+        h_w_obj = HostCrawl(env=env.name, instance=instance)
+        h_w_obj.run()
+        _p = h_w_obj.ret
+        temp['host_massage'] = ''
+        temp['mem_usage'] = _p.get('rate_memory')
+        temp['cpu_usage'] = _p.get('rate_cpu')
+        temp['disk_usage_root'] = _p.get('rate_exchange_disk')
+        temp['sys_load'] = _p.get('load')
+        temp['run_time'] = _p.get('run_time')
+        temp['host_ip'] = instance
+        temp['memory_top'] = []
+        temp['cpu_top'] = []
+        temp['kernel_parameters'] = []
+        # 总指标数/异常指标数 计算
+        tag_total_num += 23     # 当前共23个
         tag_error_num += h_w_obj.tag_error_num
-        temp['dynamic_data'] = h_w_obj.ret
-        temp['static_data'] = {}
-        h_obj = Host.objects.filter(id=instance.get('id')).first()
-        if h_obj:
-            temp['static_data']['operate_system'] = h_obj.operate_system  # 操作系统
-            temp['static_data']['host_name'] = h_obj.host_name          # 主机名
-            temp['static_data']['instance_name'] = h_obj.instance_name  # 实例名
-            temp['static_data']['ip'] = h_obj.ip                  # ip地址
-            temp['static_data']['host_agent'] = h_obj.host_agent  # 主机agent状态
-            # 监控agent状态
-            temp['static_data']['monitor_agent'] = h_obj.monitor_agent
-
+        # 操作系统
+        _h = Host.objects.filter(ip=instance).first()
+        temp['release_version'] = _h.operate_system if _h else ''
+        temp['basic'] = [
+            {"name": "IP", "name_cn": "主机IP", "value": instance},
+            {"name": "hostname", "name_cn": "主机名", "value": _h.host_name},
+            {"name": "kernel_version", "name_cn": "内核版本", "value": ""},
+            {"name": "selinux", "name_cn": "SElinux 状态", "value": ""},
+            {"name": "max_openfile", "name_cn": "最大打开文件数",
+             "value": _p.get('total_file_descriptor')},
+            {"name": "iowait", "name_cn": "IOWait",
+             "value": _p.get('rate_io_wait')},
+            {"name": "inode_usage", "name_cn": "inode 使用率",
+             "value": {"/": f"{_p.get('rate_max_disk')}%"}},
+            {"name": "now_time", "name_cn": "当前时间",
+             "value": datetime.now().strftime("%Y-%m-%d %H:%M:%S")},
+            {"name": "run_process", "name_cn": "进程数", "value": ''},
+            {"name": "umask", "name_cn": "umask",
+             "value": {"user": "commonuser", "umask": ""}},
+            {"name": "bandwidth", "name_cn": "带宽",
+             "value": _p.get('network_bytes_total')},
+            {"name": "throughput", "name_cn": "IO", "value": _p.get('disk_io')},
+            {"name": "zombies_process", "name_cn": "僵尸进程", "value": []}
+        ]
         temp_list.append(temp)
 
-    # 反填巡检记录、巡检报告 数据
-    back_fill(history_id=history_id, report_id=report_id, host_data=temp_list,
-              tag_total_num=tag_total_num, tag_error_num=tag_error_num)
+    scan_result = {
+        "all_target_num": tag_total_num,
+        "abnormal_target": tag_error_num,
+        "healthy": f"{round((tag_error_num / tag_total_num) * 100, 2)}%"
+    }
+    return scan_info, scan_result, temp_list
 
 
 @shared_task
@@ -67,24 +88,71 @@ def get_prometheus_data(env, hosts, services, history_id, report_id, handle):
     """
     异步任务：查询多巡检类型prometheus数据，组装后进行反填
     :env: 环境，例：Env 表的 queryset 对象
-    :hosts: 主机列表，例：[{"id":"主键id", "ip":"主机ip"}]
-    :services: 组件列表，例：["mysql"]
+    :hosts: 主机列表，例：["主机ip"]
+    :services: 组件列表，例：[8]
     :history_id: 巡检历史表id，例：1
     :report_id: 巡检报告表id，例：1
     :handle: 巡检类型 service-服务巡检、host-主机巡检、deep-深度巡检
     """
     try:
+        kwargs = {'history_id': history_id, 'report_id': report_id}
         if handle == 'host':
             # 主机巡检
-            get_hosts_data(env.name, hosts, history_id, report_id)
+            scan_info, scan_result, host_data = get_hosts_data(env, hosts)
+            kwargs.update({
+                'scan_info': scan_info, 'scan_result': scan_result,
+                'host_data': host_data,
+                'file_name':
+                    f"hostinspection{datetime.now().strftime('%Y%m%d')}.tar.gz"}
+            )
         elif handle == 'service':
             # 组件巡检
-            target_service_run(env, services, history_id, report_id)
+            scan_info, scan_result, serv_data = \
+                target_service_run(env, services)
+            kwargs.update({
+                'scan_info': scan_info, 'scan_result': scan_result,
+                'serv_data': serv_data,
+                'file_name':
+                    f"servinspection{datetime.now().strftime('%Y%m%d')}.tar.gz"}
+            )
         elif handle == 'deep':
             # 主机巡检
-            get_hosts_data(env.name, hosts, history_id, report_id)
+            hosts = Host.objects.filter(env=env.id).values_list('ip', flat=True)
+            if len(hosts) > 0:
+                h_info, h_result, host_data = get_hosts_data(env, list(hosts))
+            else:
+                h_info, host_data = {'host': 0}, []
+                h_result = {'all_target_num': 0, 'abnormal_target': 0}
             # 组件巡检
-            target_service_run(env, services, history_id, report_id)
+            services = Service.objects.filter(
+                service__app_type=ApplicationHub.APP_TYPE_COMPONENT)
+            services = list(services.values_list('service__id', flat=True))
+            if len(services) == 0:
+                return
+            s_info, s_result, serv_data = target_service_run(env, services)
+            scan_info = {"host": h_info.get('host'), "component": 0,
+                         "service": s_info.get('service')}
+            all_target_num = \
+                h_result.get('all_target_num') + \
+                s_result.get('all_target_num')
+            abnormal_target = \
+                h_result.get('abnormal_target') + \
+                s_result.get('abnormal_target')
+            scan_result = {
+                "all_target_num": all_target_num,
+                "abnormal_target": abnormal_target,
+                "healthy":
+                    f"{round((abnormal_target / all_target_num) * 100, 2)}%"
+            }
+            kwargs.update({
+                'scan_info': scan_info, 'scan_result': scan_result,
+                'serv_data': serv_data, 'host_data': host_data,
+                'file_name':
+                    f"deepinspection{datetime.now().strftime('%Y%m%d')}.tar.gz"}
+            )
+
+        # 反填巡检记录、巡检报告 数据
+        back_fill(**kwargs)
     except Exception as e:
         logger.error(
             f"Inspection man task failed with error: {traceback.format_exc(e)}")
@@ -109,8 +177,9 @@ def inspection_crontab(**kwargs):
             hosts, services = [], []
             if job_type in ['0', '1']:
                 # 2、查询环境下主机信息
-                hosts = Host.objects.filter(env=env.id).values('id', 'ip')
-                if not hosts:
+                hosts = Host.objects.filter(env=env.id).values_list(
+                    'ip', flat=True)
+                if len(hosts) == 0:
                     logger.error(
                         f"Inspection auto task failed with error: "
                         f"ID={env.id}环境下无主机数据")
@@ -119,9 +188,8 @@ def inspection_crontab(**kwargs):
                 services = Service.objects.filter(
                     env=env,
                     service__app_type=ApplicationHub.APP_TYPE_COMPONENT)
-                services = services.values('service_instance_name', 'ip',
-                                           'service_port', 'service__app_name')
-                if not services:
+                services = list(services.values_list('service__id', flat=True))
+                if len(services) == 0:
                     logger.error(
                         f"Inspection auto task failed with error: "
                         f"ID={env.id}环境下无组件数据")
@@ -135,7 +203,7 @@ def inspection_crontab(**kwargs):
                 start_time__day=now.day).count()
             his_dict = {
                 'inspection_name':
-                    f"{job_name}定时巡检{now.strftime('%Y%m%d')}{num+1}",
+                    f"{job_name}定时巡检{now.strftime('%Y%m%d')}{num + 1}",
                 'inspection_type': inspection_type.get(job_type),
                 'inspection_status': 1, 'execute_type': 'auto',
                 'inspection_operator': '定时服务',
--- omp_server/utils/prometheus/target_service.py
+++ omp_server/utils/prometheus/target_service.py
@@ -4,41 +4,71 @@
 # CreateDate: 2021/10/22 10:04 下午
 # Description:
 from db_models.models import Service
-from utils.prometheus.prometheus import back_fill
 from utils.prometheus.target_service_mysql import ServiceMysqlCrawl
 
 
-def target_service_run(env, services, history_id, report_id):
+def target_service_run(env, services):
     """
     组建巡检
     :env: 环境 queryset 对象
     :services: 组件服务id 列表
-    :history_id: 巡检历史表id， 作反填用
-    :report_id: 巡检报告表id，作反填用
     """
     # 查询该环境下服务
     services = Service.objects.filter(env=env, service__in=services)
     services = services.values('service_instance_name', 'ip',
                                'service_port', 'service__app_name')
-    ret = list()
-    tag_total_num = tag_error_num = 0
+    tmp_list = list()
+    tag_total_num = tag_error_num = 0   # 总指标数、异常指标数
     for i in services:
-        _ = None
+        tmp = {}
         if i.get('service__app_name') == 'mysql':
             _ = ServiceMysqlCrawl(env=env.name, instance=i.get('ip'))
-            _.run(['run_status', 'run_time', 'slow_queries',
-                   'threads_connected', 'max_connections',
-                   'threads_running', 'aborted_connects', 'qps',
-                   'slave_sql_running'])
-            tag_total_num += _.tag_total_num
+            _.run()
+            # 总指标数/异常指标数 计算
+            tag_total_num += 19
             tag_error_num += _.tag_error_num
+            tmp.update({
+                'host_ip': i.get('ip'),
+                'cluster_name': "",
+                "cpu_usage": "",
+                "log_level": "",
+                "mem_usage": "",
+                "run_time": f"{_.ret.get('run_time')}%",
+                "service_name": i.get('service_instance_name'),
+                "service_port": i.get('service_port'),
+                "service_status": _.ret.get('run_status'),
+                "service_type": "2",
+                "basic": [
+                    {"name": "max_memory", "name_cn": "最大内存", "value": ''},
+                    {"name": "port_status", "name_cn": "监听端口",
+                     "value": i.get('service_port')},
+                    {"name": "IP", "name_cn": "IP地址", "value": i.get('ip')},
+                    {"name": "process_threads", "name_cn": "线程数量",
+                     "value": ''},
+                    {"name": "conn_num", "name_cn": "连接数量",
+                     "value": _.ret.get('threads_connected')},
+                    {"name": "aborted_clients", "name_cn": "中断连接数",
+                     "value": ""},
+                    {"name": "failure_connect", "name_cn": "失败连接数",
+                     "value": ""},
+                    {"name": "slow_query", "name_cn": "慢查询",
+                     "value": _.ret.get('slow_queries')},
+                    {"name": "backup_status", "name_cn": "数据同步状态",
+                     "value": _.ret.get("slave_sql_running")}
+                ]
+            })
         elif i.get('service_instance_name') == '**':
             # TODO
             pass
 
-        i.update(**_.ret) if _ else ''
-        ret.append(i)
+        tmp_list.append(tmp) if tmp else '其他组件暂不支持'
 
-    # 反填巡检记录、巡检报告 数据
-    back_fill(history_id=history_id, report_id=report_id, serv_data=ret,
-              tag_total_num=tag_total_num, tag_error_num=tag_error_num)
+    # 扫描统计
+    scan_info = {"host": 0, "service": len(services), "component": 0}
+    # 分析结果
+    scan_result = {
+        "all_target_num": tag_total_num,
+        "abnormal_target": tag_error_num,
+        "healthy": f"{round((tag_error_num / tag_total_num) * 100, 2)}%"
+    }
+    return scan_info, scan_result, tmp_list
