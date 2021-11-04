50b33c2744d29b649aa7ac4eb6ac59f670fa2dc2
lingyang.guo <lingyang.guo@cloudwise.com>
2021-11-04 10:50:05 +0800

    仪表盘组件中增加对基础环境组件的过滤

--- omp_server/promemonitor/views.py
+++ omp_server/promemonitor/views.py
@@ -213,11 +213,14 @@ class InstrumentPanelView(GenericViewSet, ListModelMixin):
                               Service.SERVICE_STATUS_STOPPING, Service.SERVICE_STATUS_RESTARTING,
                               Service.SERVICE_STATUS_STOP]
         database_list = Service.objects.filter(service__app_type=ApplicationHub.APP_TYPE_COMPONENT).filter(
-            service__app_labels__label_name__contains="数据库").filter(service_status__in=ignore_status_list)
+            service__app_labels__label_name__contains="数据库").filter(service_status__in=ignore_status_list).filter(
+            service__is_base_env=False)
         service_list = Service.objects.filter(
-            service__app_type=ApplicationHub.APP_TYPE_SERVICE).filter(service_status__in=ignore_status_list)
+            service__app_type=ApplicationHub.APP_TYPE_SERVICE).filter(service_status__in=ignore_status_list).filter(
+            service__is_base_env=False)
         component_list = Service.objects.filter(
-            service__app_type=ApplicationHub.APP_TYPE_COMPONENT).filter(service_status__in=ignore_status_list)
+            service__app_type=ApplicationHub.APP_TYPE_COMPONENT).filter(service_status__in=ignore_status_list).filter(
+            service__is_base_env=False)
         # third_info_all = None  # TODO 暂为空
 
         host_info_all_count = len(host_list)
