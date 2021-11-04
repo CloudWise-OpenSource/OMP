c264d466a56165de6a70065715a5a5d25ab75c56
gavin.shi <gavin.shi@cloudwise.com>
2021-11-04 19:58:49 +0800

    修复ompTable问题

--- omp_web/src/utils/utils.js
+++ omp_web/src/utils/utils.js
@@ -1928,10 +1928,58 @@ export function isPassword(str) {
 }
 
 // 下载文件
-export const downloadFile = (url)=>{
+export const downloadFile = (url) => {
   let a = document.createElement("a");
   a.href = url;
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
+};
+
+// 检测对象类型
+function checkType(any) {
+  return Object.prototype.toString.call(any).slice(8, -1);
 }
+
+// 深拷贝函数
+export const clone = (any) => {
+  if (checkType(any) === "Object") {
+    // 拷贝对象
+    let o = {};
+    for (let key in any) {
+      o[key] = clone(any[key]);
+    }
+    return o;
+  } else if (checkType(any) === "Array") {
+    // 拷贝数组
+    var arr = [];
+    for (let i = 0, leng = any.length; i < leng; i++) {
+      arr[i] = clone(any[i]);
+    }
+    return arr;
+  } else if (checkType(any) === "Function") {
+    // 拷贝函数
+    return new Function("return " + any.toString()).call(this);
+  } else if (checkType(any) === "Date") {
+    // 拷贝日期
+    return new Date(any.valueOf());
+  } else if (checkType(any) === "RegExp") {
+    // 拷贝正则
+    return new RegExp(any);
+  } else if (checkType(any) === "Map") {
+    // 拷贝Map 集合
+    let m = new Map();
+    any.forEach((v, k) => {
+      m.set(k, clone(v));
+    });
+    return m;
+  } else if (checkType(any) === "Set") {
+    // 拷贝Set 集合
+    let s = new Set();
+    for (let val of any.values()) {
+      s.add(clone(val));
+    }
+    return s;
+  }
+  return any;
+};
