1c7670ef5dbd8e0960ca741f7dac458af0a1fed3
len.chen <len.chen@cloudwise.com>
2021-12-07 18:38:54 +0800

    Fixed #OMP-536 修复巡检采集bug

--- omp_server/utils/prometheus/target_service_jvm_base.py
+++ omp_server/utils/prometheus/target_service_jvm_base.py
@@ -49,12 +49,14 @@ class ServiceBase(Prometheus):
         minutes, seconds = divmod(_, 60)
         hours, minutes = divmod(minutes, 60)
         days, hours = divmod(hours, 24)
-        if int(0) > 0:
+        if int(days) > 0:
             self.ret['run_time'] = \
                 f"{int(days)}天{int(hours)}小时{int(minutes)}分钟{int(seconds)}秒"
-        else:
+        elif int(hours) > 0:
             self.ret['run_time'] = \
                 f"{int(hours)}小时{int(minutes)}分钟{int(seconds)}秒"
+        else:
+            self.ret['run_time'] = f"{int(minutes)}分钟{int(seconds)}秒"
 
     def cpu_usage(self):
         """cpu使用率"""
