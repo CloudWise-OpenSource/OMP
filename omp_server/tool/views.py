8b564deed4ab80ac25b1438dbf0030960cd591fb
flamingo <flamingo.chen@cloudwise.com>
2022-02-18 09:16:44 +0800

    Feature #OMP-536 增加小工具执行历史记录接口

--- omp_server/tool/views.py
+++ omp_server/tool/views.py
@@ -1,13 +1,15 @@
 # Create your views here.
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework.backends import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import RetrieveModelMixin
 from db_models.models import (ToolExecuteMainHistory, ToolInfo, Host, Service)
-from tool.tool_filters import ToolFilter
+from tool.tool_filters import ToolFilter, ToolInfoKindFilter
 from tool.serializers import ToolListSerializer, ToolInfoDetailSerializer, \
-    ToolTargetObjectServiceSerializer, ToolFormAnswerSerializer
+    ToolTargetObjectServiceSerializer, ToolFormAnswerSerializer, \
+    ToolExecuteHistoryListSerializer
 from app_store.views import AppStoreListView
 from utils.common.paginations import PageNumberPager
 from tool.serializers import ToolDetailSerializer, ToolFormDetailSerializer, \
@@ -87,10 +89,18 @@ class ToolTargetObjectAPIView(ListAPIView, ToolRetrieveAPIMixin):
 
 
 class ToolFormAnswerAPIView(CreateAPIView, ToolRetrieveAPIMixin):
-    permission_classes = ()
     get_description = "小工具执行表单页"
     serializer_class = ToolFormAnswerSerializer
 
     def post(self, request, *args, **kwargs):
         self.load_tool_obj()
         return self.create(request, *args, **kwargs)
+
+
+class ToolExecuteHistoryListApiView(ListAPIView):
+    get_description = "小工具执行列表页"
+    serializer_class = ToolExecuteHistoryListSerializer
+    queryset = ToolExecuteMainHistory.objects.all().select_related("tool")
+    filter_backends = (SearchFilter, OrderingFilter, ToolInfoKindFilter)
+    search_fields = ("task_name", )
+    ordering_fields = ("-start_time",)
